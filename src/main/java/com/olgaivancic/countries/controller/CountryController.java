ed0800c4d430ba0ca849e451b1f94826379e87a4
Olga Ivancic <olgap2015@users.noreply.github.com>
2018-07-04 15:39:16 +0200

    Sort by Country Name function is implemented

--- src/main/java/com/olgaivancic/countries/controller/CountryController.java
+++ src/main/java/com/olgaivancic/countries/controller/CountryController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -29,5 +30,27 @@ public class CountryController {
         modelMap.put("country", country);
         return "country-detail";
     }
+
+    @RequestMapping(value = "/countries/by-name")
+    public String listCountriesSortedByName(ModelMap modelMap) {
+        List<Country> countries = countryRepository.findAllCountries();
+        countries.sort((c1, c2) -> c1.getName().compareTo(c2.getName()));
+//        Collections.reverse(countries);
+        modelMap.put("countries", countries);
+        return "sort-by-country-name";
+    }
+
+//    courseList.sort((c1, c2) -> Integer.valueOf(c1.courseID).compareTo(c2.courseID));
+
+    @RequestMapping
+    public String listCountriesSortedByPopulation(ModelMap modelMap) {
+        List<Country> countries = countryRepository.findAllCountries();
+        // TODO:oi - figure out this method. Maybe need to implement using Comparable interface for Country class
+        countries.sort((c1, c2) -> Integer.valueOf(c1.getPopulation().compareTo(c2.getPopulation()));
+//        Collections.reverse(countries);
+        modelMap.put("countries", countries);
+        return "sort-by-country-name";
+    }
+
 
 }
